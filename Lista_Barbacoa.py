e81be19b348aa64548b39b4a7df0bd7ba9ca541c
valenro04 <valenroldanojeda@gmail.com>
2024-11-07 15:44:14 +0100

    Update Lista_Barbacoa.py
    Cambio "comida" por "carne" para especificar mas tarde otros tipos de alimentos, y "carbón" pasa a combustible

--- Lista_Barbacoa.py
+++ Lista_Barbacoa.py
@@ -1,7 +1,7 @@
 lista_barbacoa = {
-    "comida":["salchicha", "chuletas"],
+    "carne":["salchicha", "chuletas"],
     "bebida":["cocacola", "fanta naranja"],
     "cubata":["whisky_solo", "ginebra y limón"],
     "droga":["marihuana","dmt"],
-    "carbon":["encendedor","gas"] 
+    "cocina":["encendedor","gas","carbón"] 
 }
