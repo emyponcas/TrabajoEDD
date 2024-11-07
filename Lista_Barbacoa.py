9a9ce17dd1e90c6e154ec0892d132d4abe0b451f
José Luis Mármol Gómez <jluismarmolgomez@gmail.com>
2024-11-07 15:46:52 +0100

    Mod_Joselu_Lista_Barbacoa.py

--- Lista_Barbacoa.py
+++ Lista_Barbacoa.py
@@ -1,7 +1,7 @@
 lista_barbacoa = {
     "comida":["salchicha", "chuletas"],
-    "bebida":["cocacola", "fanta naranja"],
     "cubata":["whisky_solo", "ginebra y limón"],
     "droga":["marihuana","dmt"],
     "carbon":["encendedor","gas"] 
+    "porros" : ["mariguana, papeles, filtro"]
 }
